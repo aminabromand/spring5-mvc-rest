937327865f5ffb74d2703eb09fafb4a50b3b1f49
amin <amin.r.abromand@gmail.com>
2019-01-13 23:18:08 +0100

    setup jaxb

---
+++ rest-mvc/src/main/java/com/example/springmvcrestdemo/api/v1/mapper/CustomerMapper.java
@@ -0,0 +1,14 @@
+package com.example.springmvcrestdemo.api.v1.mapper;
+
+import com.example.model.CustomerDTO;
+import com.example.springmvcrestdemo.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CustomerMapper {
+
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+    CustomerDTO customerToCustomerDTO(Customer customer);
+    Customer customerDtoToCustomer(CustomerDTO customerDTO);
+}
---
+++ rest-mvc/src/test/java/com/example/springmvcrestdemo/api/v1/mapper/CustomerMapperTest.java
@@ -0,0 +1,31 @@
+package com.example.springmvcrestdemo.api.v1.mapper;
+
+import com.example.model.CustomerDTO;
+import com.example.springmvcrestdemo.domain.Customer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerMapperTest {
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+    public static final String NAME = "Joe";
+    public static final long ID = 1L;
+
+    @Test
+    public void customerToCustomerDTO() throws Exception {
+
+        //given
+        Customer customer = new Customer();
+        customer.setFirstname(NAME);
+        customer.setId(ID);
+
+        //when
+        CustomerDTO customerDTO = customerMapper.customerToCustomerDTO(customer);
+
+        //Then
+        assertEquals(Long.valueOf(ID), customerDTO.getId());
+        assertEquals(NAME, customerDTO.getFirstname());
+    }
+
+}
